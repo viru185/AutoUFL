811be84842e5254a4c420e83de66ad012fcc9861
Viren Hirpara <62017836+viru185@users.noreply.github.com>
2026-04-01 10:31:00 +0530

    refactor(init): change attributes name for better readability

--- src/clients/__init__.py
+++ src/clients/__init__.py
@@ -1,6 +1,7 @@
 import importlib
 import os
 import sys
+from typing import Type
 
 from src.logger import logger
 
@@ -16,20 +17,20 @@ else:
 
 try:
     # Construct the full module path
-    # This points to 'clients.client_one.processor'
     module_path = f"src.clients.{client_name}.processor"
 
     # Dynamically import the module
-    module = importlib.import_module(module_path)
+    processor = importlib.import_module(module_path)
 
-    # Pull the 'Processor' class/function into the local namespace
-    ExcelProcessor = getattr(module, "ExcelProcessor")
+    # Pull class/function into the local namespace
+    ExcelProcessor = getattr(processor, "ExcelProcessor")
+    ProcessingError: Type[Exception] = getattr(processor, "ProcessingError")
 
 except (ImportError, AttributeError):
-    logger.exception(
-        f"Could not load Processor for '{client_name}'."
+    logger.critical(
+        f"Could not load required modules for '{client_name}'."
         f"Ensure 'clients/{client_name}/processor.py' exists and contains a 'Processor' class."
     )
     sys.exit(1)
 
-__all__ = ["ExcelProcessor"]
+__all__ = ["ExcelProcessor", "ProcessingError"]
